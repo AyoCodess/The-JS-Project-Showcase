60a060bb48e2fb37bf95372dcff8381c61032335
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-06-21 20:29:24 +0200

    implemened basic fetch request api call to generate to quotes to the console

---
+++ quote-generator/quote-generator-scrip-local-quotes.js
@@ -0,0 +1,10 @@
+// // if i have an local object/api i can edit my own quotes
+
+// function newLocalQuote() {
+//   //? pick a random quote from api array
+
+//   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//   console.log(quote);
+// }
+
+// newLocalQuote();
--- quote-generator/quote-generator-script.js
+++ quote-generator/quote-generator-script.js
@@ -1,1 +1,28 @@
 // ? getting quotes from API
+
+let apiQuotes = [];
+
+//? show new quote
+
+function newQuote() {
+  //? pick a random quote from api array
+
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+async function getQuotes() {
+  const apiURL = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    alert(`Sorry we encountered this ${error}, please try again.`);
+  }
+}
+
+//? on load
+
+getQuotes();
