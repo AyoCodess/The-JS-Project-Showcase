4d8de9c75bba4c354d8cf2795b9d47ca5bb31363
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-06-24 14:54:26 +0200

    used accessibility friendly method to remove focus state on button in quote ganerator

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ window.addEventListener("load", (event) => {
   console.log("page is fully loaded");
   loader1.hidden = true;
 });
+
+const newQuoteBtn = document.getElementById(`new-quote`);
+
+newQuoteBtn.addEventListener(`mousedown`, function (e) {
+  e.preventDefault();
+});
