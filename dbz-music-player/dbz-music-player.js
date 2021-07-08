f36b9352b1618d8c80812791fcb2481a91b4ca89
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-08 12:26:14 +0200

    added music object to js and songs

--- dbz-music-player/dbz-music-player.js
+++ dbz-music-player/dbz-music-player.js
@@ -1,7 +1,50 @@
+const image = document.querySelector(`img`);
+const title = document.getElementById(`title`);
+const artist = document.getElementById(`artist`);
 const music = document.querySelector(`audio`);
 const prevBtn = document.getElementById(`prev`);
 const playBtn = document.getElementById(`play`);
 const nextBtn = document.getElementById(`next`);
+
+//? Music
+
+const songs = [
+  {
+    name: `BruceFaulconer-Dragonball-Z`,
+    displayName: `Dragonball Z - Intro`,
+    artist: `Bruce Faulconer`,
+  },
+  {
+    name: `BruceFaulconer-SSJ-Transformation`,
+    displayName: `SSJ Transformation`,
+    artist: `Bruce Faulconer`,
+  },
+  {
+    name: `BruceFaulconer-18-Vegetas-Theme`,
+    displayName: `Vegeta's Theme`,
+    artist: `Bruce Faulconer`,
+  },
+  {
+    name: `BruceFaulconer-03-Trunks-Appears`,
+    displayName: `Trunks Appears`,
+    artist: `Bruce Faulconer`,
+  },
+  {
+    name: `BruceFaulconer-Gohan-fights-Frieza`,
+    displayName: `Gohan Fights Frieza`,
+    artist: `Bruce Faulconer`,
+  },
+  {
+    name: `BruceFaulconer-Trunks-Compendium-Prelude-to-Conflict`,
+    displayName: `Dragonball Z - Intro 2 (Prelude To Conflict)`,
+    artist: `Bruce Faulconer`,
+  },
+  {
+    name: `BruceFaulconer-20-Vegeta-Super-Saiyan`,
+    displayName: `Vegeta Super Saiyan`,
+    artist: `Bruce Faulconer`,
+  },
+];
 
 //? check if playing
 let isPlaying = false;
@@ -28,3 +71,20 @@ function pauseSong() {
 playBtn.addEventListener(`click`, () => {
   isPlaying ? pauseSong() : playSong();
 });
+
+//? Update DOM
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpeg`;
+}
+
+//? on load - select first song
+
+loadSong(songs[0]);
+
+//? Event Listeners
+
+prevBtn;
