27b8f4ae0bb06777d86072432dfb7a89692725e3
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-08 18:25:05 +0200

    bug fix

--- dbz-music-player/dbz-music-player.js
+++ dbz-music-player/dbz-music-player.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const image = document.querySelector(`img`);
+const image = document.querySelector(`.img`);
 const title = document.getElementById(`title`);
 const artist = document.getElementById(`artist`);
 const music = document.querySelector(`audio`);
