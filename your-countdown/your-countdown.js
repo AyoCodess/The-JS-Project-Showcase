8bf3ba676a38393ab8e1a90a9b3e0e99527dadcb
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-09 13:38:56 +0200

    date setup and from submit operaiton on countdown project

--- your-countdown/your-countdown.js
+++ your-countdown/your-countdown.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const inputContainer = document.getElementById(`input=container`);
+const countdownForm = document.getElementById(`countdownForm`);
+const dateEL = document.getElementById(`date-picker`);
+
+let countdownTitle = ``;
+let countdownDate = ``;
+
+//? set dat input minimum with todays date
+
+const today = new Date().toISOString().split(`T`)[0];
+
+//? event listeners
+
+//? takes values from form input
+function updateCountdown(e) {
+  e.preventDefault();
+
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+
+  console.log(countdownDate, countdownTitle);
+}
+
+countdownForm.addEventListener(`submit`, updateCountdown);
